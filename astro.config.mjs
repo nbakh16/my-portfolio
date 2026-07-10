649d09f9865eb4f7b3c79e0df9ab6756aad9d1a8
Lesli Ariana <97137980+lesliariana@users.noreply.github.com>
2026-07-09 21:19:08 -0600

    Add deploy configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lesliariana.github.com',
-  // base: '/',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   },
